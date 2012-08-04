d1e7153f312fc0efe846816f71bd0532050cbe50
Kaustubh Bhalerao <bhalerao.1@gmail.com>
2012-08-04 00:04:30 -0500

    Initial Commit

---
+++ SCNSim/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SCNSim
+//
+//  Created by Kaustubh Bhalerao on 8/4/12.
+//  Copyright (c) 2012 Kaustubh Bhalerao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
